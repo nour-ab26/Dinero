ecab8c82bf6e2d0d377539d8632b516296ebee7b
nour-ab26 <147323825+nour-ab26@users.noreply.github.com>
2024-07-12 00:00:56 +0100

    commit

--- app.js
+++ app.js
@@ -1,26 +1,100 @@
 // Importing libraries that we installed using npm
 const express = require("express");
 const bcrypt = require("bcrypt");
-
+const passport = require("passport");
+//const initializePassport = require( "./config/passport").initialize;
 
 const app = express();
 const port=3000;
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({extended:false}));  
 
-// Route to render homa page 
+// Routes
 app.get('/', (req, res) => {
   res.render("acceuil.ejs");
 })
 
-// Route to render the login page
 app.get('/login', (req, res) => {
   res.render('login.ejs');
 });
 
-// Route to render the signup page
+app.get('/homePage', (req, res) => {
+  res.render('homePage.ejs');
+});
+
+
 app.get('/signUp', (req, res) => {
   res.render('signUp.ejs');
 });
+
+app.get('/transactions', (req, res) => {
+  res.render('transactions.ejs');
+});
+
+app.get('/expense', (req, res) => {
+  res.render('expense.ejs');
+});
+
+app.get('/income', (req, res) => {
+  res.render('income.ejs');
+});
+
+app.get('/transafter', (req, res) => {
+  res.render('transafter.ejs');
+});
+
+app.get('/budget', (req, res) => {
+  res.render('budget.ejs');
+});
+
+app.get('/newbudget', (req, res) => {
+  res.render('newBud.ejs');
+});
+
+app.get('/FoodBudget', (req, res) => {
+  res.render('FoodBudget.ejs');
+});
+
+app.get('/bill', (req, res) => {
+  res.render('bill.ejs');
+});
+
+app.get('/newbill', (req, res) => {
+  res.render('newbill.ejs');
+});
+
+app.get('/ElecBill', (req, res) => {
+  res.render('ElecBill.ejs');
+});
+
+app.get('/goal', (req, res) => {
+  res.render('goal.ejs');
+});
+
+app.get('/newgoal', (req, res) => {
+  res.render('newgoal.ejs');
+});
+
+app.get('/phonegoal', (req, res) => {
+  res.render('phonegoal.ejs');
+});
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard.ejs');
+});
+
+app.get('/newAccount', (req, res) => {
+  res.render('newAccount.ejs');
+});
+
+app.get('/addgoal', (req, res) => {
+  res.render('addgoal.ejs');
+});
+
+app.get('/finalgoal', (req, res) => {
+  res.render('finalgoal.ejs');
+});
+// End of routes
 
 // In-memory
 const users =[];
@@ -29,7 +103,7 @@ app.use(express.json());  // Middleware to parse JSON in the request body
 
 app.post( "/signUp", async (req, res) => {   // Route for registration of new user
     try {
-        const {email,password} = req.body ;     // Extract email and password from the request
+        const {firstName,lastName,email,password} = req.body ;     // Extract email and password from the request
 
        // Check if the user already exists
        const findUser = users.find((data) => email == data.email);
@@ -39,9 +113,10 @@ app.post( "/signUp", async (req, res) => {   // Route for registration of new us
 
        // Hash password
        const hashedPassword = await bcrypt.hash(password,10);
-       users.push({ email, password:hashedPassword });
-       console.log(users);
-       res.status(201).send("Registered successfully !");
+       users.push({  id: Date.now().toString(), firstName, lastName, email, password:hashedPassword });
+       console.log(users); // Display newly registered in the console
+       //res.status(201).send("Registered successfully !");
+       res.redirect("/login")
     } catch (err) {
           res.status(500).send({ message: err.message});
     }
@@ -60,7 +135,8 @@ app.post("/login",async (req,res)=>{      // Route for login authentication
       // Compare passwords
       const passwordMatch =await bcrypt.compare(password,findUser.password);
       if (passwordMatch){
-        res.status(201).send("Logged in successfully !");
+        //res.status(201).send("Logged in successfully !");
+        res.redirect("/newAccount"); // Redirect to home page on successful login
       }else{ 
         res.status(400).send(" Wrong email or password !");}
     } catch (err){
